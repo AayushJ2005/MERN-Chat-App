c2b5d64e40e8515281cb46aa550d7d83a9e65757
AayushJ2005 <aayushjindal2017@gmail.com>
2026-05-27 21:29:28 +0530

    Fixed Nodemailer timeout issue

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 // Yeh transporter hamare backend ko Gmail se connect karega
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
